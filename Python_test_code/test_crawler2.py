9da340862da40dfb405e34f23b2a17d2f4dc6179
Michael Gwyn <Mikegwyn17@gmail.com>
2015-05-22 11:48:55 -0400

    made changes to web_crawler 2. Now it ends on a memory error

--- Python_test_code/test_crawler2.py
+++ Python_test_code/test_crawler2.py
@@ -1,50 +1,64 @@
 import requests
-import re
 from bs4 import BeautifulSoup
 
+
 def crawler(domain):
+    start_page = "http://" + domain
     to_search = []
     searched = []
-    to_search.append(domain)
+    to_search.append(start_page)
     while (len(to_search) != 0):
         current_page = to_search.pop(0)
-        print(current_page)
         if current_page in searched:
+            # print(current_page + " already searched")
             continue
-        source_code = requests.get(current_page)
-        print(source_code.status_code)
+        try:
+            source_code = requests.get(current_page,timeout = 1)
+        except TimeoutError:
+            print ("Timeout error on url: " + current_page)
+            searched.append(current_page)
+            continue
+        except ConnectionError:
+            print ("Connection error on url: " + current_page)
+            searched.append(current_page)
+            continue
+        except :
+            print ("some error occurred at url: " + current_page)
+            searched.append(current_page)
+            continue
         searched.append(current_page)
+        print(current_page)
+        print(source_code.status_code)
+        if not domain in current_page:
+            print (current_page + " not contained in domain: " + domain)
+            continue
         plain_text = source_code.text
         reaper = BeautifulSoup(plain_text)
         for link in reaper.findAll('a'):
-            # if (link.has_attr('href')):
-            #     print(link['href'])
             if link.get('href') is None:
                 break
             elif link.get('href') == '':
                 break
             elif link.get('href')[0] == '/':
-                puppies = domain + link.get('href')
-                # print(link.get('href'))
+                puppies = start_page + link.get('href')
             elif link.get('href')[0] == '?':
-                puppies = domain + link.get('href')
+                puppies = start_page + link.get('href')
             elif link.get('href')[0] == '#':
-                puppies = domain + link.get('href')
+                puppies = start_page + link.get('href')
             elif link.get('href')[0] == 'h':
                 puppies = link.get('href')
             elif link.get('href')[0] == '@':
-                puppies = domain + link.get('href')
+                puppies = start_page + link.get('href')
             elif link.get('href')[0:2] == '..':
-                puppies = domain + link.get('href')
+                puppies = start_page + link.get('href')
             else:
-                break
+                puppies = link.get('href')
             to_search.append(puppies)
-    for link in searched:
-        print(link)
-    for link in to_search:
-        print(link)
 
-crawler("https://spsu.edu")
+crawler("spsu.edu/")
+
+
+
 
 
 
