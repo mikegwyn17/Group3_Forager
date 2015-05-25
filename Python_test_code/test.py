bc349ecbce7eeb6defedcc1be4f09a69e0048319
Michael Gwyn <mikegwyn17@gmail.com>
2015-05-25 16:54:49 -0400

    changed WebCrawler to use sets instead of lists

---
+++ Python_test_code/test.py
@@ -0,0 +1,7 @@
+url = "https://spsu.edu"
+s = set()
+s.add(url)
+print(s)
+no = s.pop()
+print(s)
+print (no)
--- Python_test_code/test_crawler2.py
+++ Python_test_code/test_crawler2.py
@@ -2,73 +2,88 @@ import requests
 import requests.exceptions
 from bs4 import BeautifulSoup
 import sqlite3
+import socket
 
 def crawler(domain):
-    db = sqlite3.connect(":memory:")
-    c = db.cursor()
+    # db = sqlite3.connect("pydb.db")
+    # c = db.cursor()
     # c.execute("DROP TABLE IF EXISTS searched")
-    c.execute("CREATE TABLE searched (link text) ")
+    # c.execute("CREATE TABLE searched (link text) ")
     start_page = "http://" + domain
-    to_search = []
-    to_search.append(start_page)
+    to_search = set()
+    searched = set()
+    to_search.add(start_page)
     while len(to_search) != 0:
-        current_page = to_search.pop(0)
-        sql2 = "SELECT * FROM searched WHERE link = ? LIMIT 1"
-        c.execute(sql2,[(current_page)])
-        temp = c.fetchone()
-        if temp == None:
-            pass
-        elif temp[0] == current_page:
+        current_page = to_search.pop()
+        if current_page in searched:
             continue
+        # sql2 = "SELECT * FROM searched WHERE link = ? LIMIT 1"
+        # c.execute(sql2,[(current_page)])
+        # temp = c.fetchone()
+        # if temp == None:
+        #     pass
+        # elif temp[0] == current_page:
+        #     continue
         try:
             source_code = requests.get(current_page,timeout = 1,stream=True)
             contentType = source_code.headers.get('content-type')
-            if not 'text/html' in contentType and not 'application/xhtml+xml' in contentType and not 'application/xml' in contentType:
+            if contentType is None:
+                print("None content type: " + current_page)
+                continue
+            elif not 'text/html' in contentType and not 'application/xhtml+xml' in contentType and not 'application/xml' in contentType:
                 print(contentType)
                 print ("Wrong content-type on url, not parsing: " + current_page)
                 print(source_code.status_code)
-                sql = "INSERT INTO searched (link) VALUES (?)"
-                c.execute(sql,(current_page,))
-                db.commit()
+                # sql = "INSERT INTO searched (link) VALUES (?)"
+                # c.execute(sql,(current_page,))
+                # db.commit()
+                searched.add(current_page)
                 continue
             response = ''
             for chunk in source_code.iter_content(chunk_size=1024,decode_unicode=True):
                 if(chunk):
                     response += chunk
-
         except requests.exceptions.ConnectTimeout:
             print ("Timeout error on url: " + current_page)
-            sql = "INSERT INTO searched (link) VALUES (?)"
-            c.execute(sql,(current_page,))
-            db.commit()
+            # sql = "INSERT INTO searched (link) VALUES (?)"
+            # c.execute(sql,(current_page,))
+            # db.commit()
+            searched.add(current_page)
             continue
         except requests.exceptions.ConnectionError:
             print ("Connection error on url: " + current_page)
-            sql = "INSERT INTO searched (link) VALUES (?)"
-            c.execute(sql,(current_page,))
-            db.commit()
+            # sql = "INSERT INTO searched (link) VALUES (?)"
+            # c.execute(sql,(current_page,))
+            # db.commit()
+            searched.add(current_page)
             continue
         except requests.exceptions.TooManyRedirects :
             print ("Too many redirects from url: " + current_page)
-            sql = "INSERT INTO searched (link) VALUES (?)"
-            c.execute(sql,(current_page,))
-            db.commit()
+            # sql = "INSERT INTO searched (link) VALUES (?)"
+            # c.execute(sql,(current_page,))
+            # db.commit()
+            searched.add(current_page)
             continue
-        except :
-            print ("some error occurred at url: " + current_page)
-            sql = "INSERT INTO searched (link) VALUES (?)"
-            c.execute(sql,(current_page,))
-            db.commit()
+        except socket.timeout:
+            print ("timeout at url: " + current_page)
+            # sql = "INSERT INTO searched (link) VALUES (?)"
+            # c.execute(sql,(current_page,))
+            # db.commit()
+            searched.add(current_page)
             continue
-        sql = "INSERT INTO searched (link) VALUES (?)"
-        c.execute(sql,(current_page,))
-        db.commit()
+        except requests.exceptions.ReadTimeout:
+            print ("Read timeout on url: " + current_page)
+        # sql = "INSERT INTO searched (link) VALUES (?)"
+        # c.execute(sql,(current_page,))
+        # db.commit()
+        except requests.exceptions.InvalidSchema:
+            print("Invalid Url: " + current_page)
+        searched.add(current_page)
         print(current_page)
         print(source_code.status_code)
-        if not domain in current_page:
-            print (current_page + " not contained in domain: " + domain)
+        if domain not in current_page:
+            print(current_page + " not contained in domain: " + domain)
             continue
-
         plain_text = response#source_code.text
         reaper = BeautifulSoup(plain_text)
         for link in reaper.findAll('a'):
@@ -85,17 +100,17 @@ def crawler(domain):
                 puppies = start_page + href
             elif href[0] == '#':
                 break
-            elif href[0] == 'h':
-                puppies = href
-            #elif href[0] == '@':
-            #    puppies = start_page + href
             elif href[0:2] == '..':
                 puppies = start_page + href
-            else:
+            elif href[0:4] == 'http' or href[0:5] == 'https':
                 puppies = href
-            to_search.append(puppies)
+            else:
+                puppies = start_page + href
+            to_search.add(puppies)
 
 crawler("spsu.edu/")
+
+
 
 
 
