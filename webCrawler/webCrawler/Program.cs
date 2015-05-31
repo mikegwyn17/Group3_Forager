c49b34b81bd84e3a98e91a74b5bfa5b4354003e2
Michael <mikegwyn17@gmail.com>
2015-05-30 23:37:23 -0400

    c# web crawler mark 1

--- webCrawler/webCrawler/Program.cs
+++ webCrawler/webCrawler/Program.cs
@@ -13,18 +13,24 @@ namespace webCrawler
     {
         static void Main(string[] args)
         {
-            HttpWebResponse response = default(HttpWebResponse);
-            HttpWebRequest request = default(HttpWebRequest);
             string current_page = "";
-            string domain = "spsu.edu/";
+            string domain = "spsu.edu";
             string start_page = "https://" + domain;
             List<string> to_search = new List<string>();
             List<string> searched = new List<string>();
             List<string> parents = new List<string>();
             string puppies = "";
             string href = "";
+            string content = "";
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(start_page);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader stream = new StreamReader(response.GetResponseStream());
+            string source_code = stream.ReadToEnd();
+            HtmlDocument doc = new HtmlDocument();
+            doc.OptionReadEncoding = false;
             to_search.Add(start_page);
-            while (to_search.Count() != 0)
+
+            while (to_search.Count > 0)
             {
                 current_page = to_search.ElementAt(0);
                 to_search.RemoveAt(0);
@@ -32,52 +38,100 @@ namespace webCrawler
                 {
                     parents.RemoveAt(0);
                 }
-                foreach (string link in searched)
+                foreach (string link in searched) // add things to searched
                 {
                     if (link == current_page)
                     {
                         continue;
                     }
                 }
-                // parse dat html
-                HtmlWeb html_web = new HtmlAgilityPack.HtmlWeb();
-                HtmlDocument doc = html_web.Load(current_page);
+                //request = (HttpWebRequest)HttpWebRequest.Create(current_page);
+                //request.Method = "HEAD";
+                ////response.Dispose();
+                //try
+                //{
+                //    response = (HttpWebResponse)request.GetResponse();
+                //    //request.EndGetResponse();
+
+                //}
+                //catch (System.Net.WebException ex)
+                //{
+                //    var status = response.StatusCode;
+                //    switch(status.ToString())
+                //    {
+                //        case "NotFound":
+                //            Console.WriteLine("Huzzah!");
+                //            break;
+                //    }
+                //    Console.WriteLine(ex);
+                //    continue;
+                //}
+                //var status_code = response.StatusCode;
+                //Console.WriteLine(status_code.ToString());
                 try
                 {
+                    request = (HttpWebRequest)HttpWebRequest.Create(current_page);
                     response = (HttpWebResponse)request.GetResponse();
-                    Console.WriteLine(response.StatusCode);
                 }
-                catch (Exception ex)
+                catch (System.Net.WebException ex)
                 {
-                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex.Status);
+                    continue;
                 }
-                
+                var status = response.StatusCode;
+                Console.WriteLine(status);
                 searched.Add(current_page);
-                foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//a[@href]"))
+                content = response.ContentType;
+                if (!(content.Contains("text/html") || content.Contains("application/xhtml+xml") || content.Contains("application/xml")))
                 {
-                    HtmlAttribute att = link.Attributes["href"];
-                    href = att.Value;
-                    if (href == null)
-                        break;
-                    else if (href == "")
-                        break;
-                    else if (href.Contains("mailto:"))
-                        break;
-                    else if (href.ElementAt(0) == '/')
-                        puppies = start_page + href;
-                    else if (href.ElementAt(0) == '?')
-                        puppies = start_page + href;
-                    else if (href.ElementAt(0) == '#')
-                        break;
-                    else if (href.Contains("http"))
-                        puppies = href;
-                    else if (href.Contains("javascript"))
-                        break;
-                    else puppies = start_page + href;
-                    to_search.Add(puppies);
-                    parents.Add(current_page);
-                    Console.WriteLine(puppies);
+                    Console.WriteLine("Invalid Content type " + current_page);
+                    continue;
                 }
+                //if (!current_page.Contains(domain)) //use host?
+                //{
+                //    Console.WriteLine("Off domain " + current_page);
+                //    continue;
+                //}
+                if (!request.Host.Equals(domain))
+                {
+                    Console.WriteLine("Off domain " + current_page);
+                    continue;
+                }
+                // start to parse html 
+                stream = new StreamReader(response.GetResponseStream());
+                source_code = stream.ReadToEnd();
+                doc.LoadHtml(source_code);
+                Console.WriteLine(current_page);
+                var links = doc.DocumentNode.SelectNodes("//a[@href]");
+                if (links != null)
+                {
+                    foreach (HtmlNode link in links)
+                    {
+                        HtmlAttribute att = link.Attributes["href"];
+                        href = att.Value;
+                        if (href == null)
+                            break;
+                        else if (href == "")
+                            break;
+                        else if (href.Contains("mailto:"))
+                            break;
+                        else if (href.ElementAt(0) == '/')
+                            puppies = start_page + href;
+                        else if (href.ElementAt(0) == '?')
+                            puppies = start_page + href;
+                        else if (href.ElementAt(0) == '#')
+                            break;
+                        else if (href.Contains("http"))
+                            puppies = href;
+                        else if (href.Contains("javascript"))
+                            break;
+                        else puppies = start_page + href;
+                        to_search.Add(puppies);
+                        parents.Add(current_page);
+                        //Console.WriteLine(puppies);
+                    }
+                }
+                response.Close();
             }
         }
     }
